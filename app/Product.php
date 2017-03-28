6ce6e7865ae7b6a4c9860e3bc1d503ede2a0b9dc
NORYB2007 <bygishalom@gmail.com>
2017-03-27 20:21:21 -0500

    CREAMOS RELACIONES, TABLAS

---
+++ app/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable=[
+    	'name','short','body','image','user_id'
+    ];
+
+    public function user()
+    {
+		return $this->belongsTo(User::class);    	
+    }
+}
