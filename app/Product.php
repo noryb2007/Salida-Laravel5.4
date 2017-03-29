421c671e3bcd6b75967a7a4ff1e2ee22e5a5258b
NORYB2007 <bygishalom@gmail.com>
2017-03-28 20:44:58 -0500

    Cambio Products

--- app/Product.php
+++ app/Product.php
@@ -14,4 +14,9 @@ class Product extends Model
     {
 		return $this->belongsTo(User::class);    	
     }
+
+    public function getTextShortAttribute()
+    {
+    	return substr($this->short, 0, 80) . '...';
+    }
 }
